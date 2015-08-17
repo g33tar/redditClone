ecce309a3499c8edea23827203a3c4bbe0568de0
Tara Gee <geetar40@gmail.com>
2015-08-16 22:05:30 -0600

    stuff

--- app.js
+++ app.js
@@ -3,6 +3,24 @@ var app = angular.module("redditApp", []);
 app.controller("redditController", function($scope){
     $scope.posts = [];
     $scope.addPosts = function(){
-    $scope.posts.push({title: $scope.title, author: $scope.author, image: $scope.image, description: $scope.description, date: $scope.date, comments: $scope.comments, votes: $scope.votes})
+    $scope.posts.push({title: $scope.title, author: $scope.author, image: $scope.image, description: $scope.description})
+    $scope.title = "";
+    $scope.author = "";
+    $scope.image = "";
+    $scope.description = "";
     }
+    $scope.upVote = function(){
+      $scope.vote++;
+    }
+    $scope.downVote = function(){
+      $scope.vote--;
+    }
+    $scope.vote = 0;
+    $scope.date = Date.now();
+  //   $scope.comments = [];
+  //   $scope.addComments = function(){
+  //   $scope.comments.push({user: $scope.user, comments: $scope.comments})
+  //   $scope.user = "";
+  //   $scope.comments = "";
+  // }
 })
